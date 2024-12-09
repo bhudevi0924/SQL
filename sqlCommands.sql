9303d1ec2e08985e9b340b3a8a832c4edc0aa029
Bhudevi Dobbala <bhudevi2461@gmail.com>
2024-12-09 13:38:43 +0530

    database design documentation

--- sqlCommands.sql
+++ sqlCommands.sql
@@ -34,3 +34,17 @@ ALTER TABLE Employees ALTER COLUMN empid varchar; /* changes datatype of the col
 
 INSERT INTO Employees values(174, 'Bhudevi Dobbala', '9515604545', 'Hyderabad'); /* insert data into table */
 INSERT INTO Employees (empid, empname) values(174, 'Bhudevi Dobbala');  /* insert data to specific columns */
+
+select * from Employees                 /* gets all data in the table */
+select empid, empname from Employees    /* gets only specified columns data from the table */
+select distinct city from Employees;   /* selects all the different cities from the table */
+select count(*) from Employees;        /* gives no.of rows in the table */
+select * from employees where city='Hyderabad'  /* gives records having city as hyderabad */
+select * from employees order by empid          /* gives records ordered by empid */
+select * from employees where city is null      /* selects all the records having city value as NULL */
+select * from employees where city is not null  /* selects all the records not having city values as NULL */
+
+update employees set city='local' where city is null /* updates value of city as local in the table where ever the city value is null */
+
+DELETE FROM employees WHERE city='local';     /* delete the records from the table where city value is local */
+DELETE from employees;                        /* delete all the records the table */
