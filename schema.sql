157fa51046be422362faf14659b064a14839e001
Bhudevi Dobbala <bhudevi2461@gmail.com>
2024-12-10 18:19:28 +0530

    views, indexes, CTE, and recursive CTE

--- schema.sql
+++ schema.sql
@@ -112,3 +112,103 @@ END;
 
 /*record will be inserted into department  logs table when ever department is updated in employees table*/
 update employees set departmentid=4 where empid=1
+
+/*creating a view to display all the employee details*/
+CREATE VIEW EmployeeDetails AS
+SELECT 
+    e.EmpID,
+    e.FirstName,
+    e.LastName,
+    d.DepartmentName,
+    e.DepartmentID
+FROM 
+    Employees e
+LEFT JOIN 
+    Departments d ON e.DepartmentID = d.DepartmentID;
+
+/*executing the view*/
+SELECT * FROM EmployeeDetails;
+
+/*updating the view to change the fields in the view*/
+ALTER VIEW EmployeeDetails AS
+SELECT 
+    e.EmpID,
+    e.FirstName,
+    e.LastName,
+    d.DepartmentName
+FROM 
+    Employees e
+INNER JOIN 
+    Departments d ON e.DepartmentID = d.DepartmentID;
+
+/*update base table data through view*/
+UPDATE EmployeeDetails
+SET FirstName = 'Devi'
+WHERE EmpID = 1;
+
+/*drop the view*/
+DROP VIEW EmployeeDetails;
+
+/*create clustered index*/
+CREATE CLUSTERED INDEX IX_Employees_DepartmentID 
+ON Employees(DepartmentID);
+
+/*create non clustered index*/
+CREATE NONCLUSTERED INDEX IX_Employees_LastName 
+ON Employees(LastName);
+
+/*indexed view*/
+CREATE VIEW EmployeeSummary WITH SCHEMABINDING AS
+SELECT 
+    e.EmpID,
+    e.FirstName,
+    e.LastName,
+    e.DepartmentID
+FROM 
+    dbo.Employees e;
+
+/*creating clustered index on the view*/
+CREATE UNIQUE CLUSTERED INDEX IX_EmployeeSummary ON EmployeeSummary(EmpID);
+
+CREATE TABLE EmployeesDetails (
+    EmployeeID INT PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    ManagerID INT NULL,
+    Department NVARCHAR(50) NOT NULL
+);
+
+INSERT INTO EmployeesDetails (EmployeeID, Name, ManagerID, Department)
+VALUES
+    (1, 'Bhudevi', NULL, 'HR'),
+    (2, 'Devi', 1, 'IT'),
+    (3, 'Sana', 2, 'IT'),
+    (4, 'Shivani', 2, 'IT'),
+    (5, 'Sravanthi', 1, 'HR'),
+    (6, 'Madhuri', 5, 'HR');
+
+/*create CTE*/
+WITH IT_Employees AS (
+    SELECT EmployeeID, Name, Department
+    FROM EmployeesDetails
+    WHERE Department = 'IT'
+)
+SELECT *
+FROM IT_Employees;
+
+/* create recursive CTE*/
+WITH EmployeeHierarchy (EmployeeID, Name, ManagerID, Level)
+AS (
+    SELECT EmployeeID, Name, ManagerID, 1 AS Level
+    FROM EmployeesDetails
+    WHERE ManagerID IS NULL
+
+    UNION ALL
+
+    -- Recursive query: Employees reporting to current level employees
+    SELECT e.EmployeeID, e.Name, e.ManagerID, eh.Level + 1
+    FROM EmployeesDetails e
+    INNER JOIN EmployeeHierarchy eh
+    ON e.ManagerID = eh.EmployeeID
+)
+SELECT *
+FROM EmployeeHierarchy;
